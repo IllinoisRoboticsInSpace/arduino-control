dd19758cc96dcaeec263400c12ff819d316900e3
Filotti99 <filippotagliacarne@gmail.com>
2019-12-02 21:04:24 -0600

    Updated Arduino serial

--- RemoteControl/ArduinoSerial.py
+++ RemoteControl/ArduinoSerial.py
@@ -5,6 +5,8 @@
 import serial
 import time
 
+debug = True
+
 def sendData(ser, motN, spd):
 	#ser is the serial object
 	#motn is the motor number; 1-2 are for the first sabetooth, 3-4 for the second and so on...
@@ -13,6 +15,13 @@ def sendData(ser, motN, spd):
 	cmd = bytes("#"+str(motN)+"$"+str(speed),'utf-8')
 	#time.sleep(1)
 	ser.write(cmd)
-	print(cmd)
-	print(str(ser.readline(), 'utf-8'))
+	if debug: print(cmd)
+	#print(str(ser.readline(), 'utf-8'))
+
+def beginTransmission(ser):
+	ser.write(bytes("begin",'utf-8'))
+	if debug: print("Initialization command sent\nAwaiting response...")
+	if str(ser.readline(), 'utf-8') == "done":
+		print("Initialization compete")
+		
 
